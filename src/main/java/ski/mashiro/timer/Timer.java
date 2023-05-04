8c1ec677c66895a83c215d581a74e71fd85dd865
mashitot <mashirotj@gmail.com>
2023-05-04 22:18:38 +0800

    节假日停止推送

--- src/main/java/ski/mashiro/timer/Timer.java
+++ src/main/java/ski/mashiro/timer/Timer.java
@@ -1,11 +1,14 @@
 package ski.mashiro.timer;
 
 import net.mamoe.mirai.Bot;
+import ski.mashiro.constant.StatusCodeConstants;
 import ski.mashiro.data.CourseData;
 import ski.mashiro.dto.CourseDto;
+import ski.mashiro.dto.Result;
 import ski.mashiro.file.ConfigFile;
 import ski.mashiro.util.CourseUtil;
 import ski.mashiro.util.FormatterUtil;
+import ski.mashiro.util.HttpUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,25 +25,30 @@ import java.util.concurrent.TimeUnit;
  * @author MashiroT
  */
 public class Timer {
-    public static final ScheduledThreadPoolExecutor POOL = new ScheduledThreadPoolExecutor(2, r -> {
+    public static boolean isHoliday = false;
+    public static final ScheduledThreadPoolExecutor POOL = new ScheduledThreadPoolExecutor(3, r -> {
         Thread.currentThread().setName("TimerPool");
         return new Thread(r);
     });
 
     public static void startTimer() {
-        LocalDateTime nextDay = LocalDate.now().plusDays(1).atTime(6, 0);
+        isHoliday();
+        LocalDateTime nextDay = LocalDate.now().plusDays(1).atTime(0, 0);
         LocalDateTime nextMinutes = LocalDateTime.now().plus(1, ChronoUnit.MINUTES).with(ChronoField.SECOND_OF_MINUTE, 0);
-        courseUpdater(nextDay.toInstant(ZoneOffset.of("+8")).toEpochMilli() - System.currentTimeMillis());
+        courseUpdater(nextDay.plusHours(6).toInstant(ZoneOffset.of("+8")).toEpochMilli() - System.currentTimeMillis());
         nextCourseChecker(nextMinutes.toInstant(ZoneOffset.of("+8")).toEpochMilli() - System.currentTimeMillis());
+        holidayChecker(nextDay.toInstant(ZoneOffset.of("+8")).toEpochMilli() - System.currentTimeMillis());
     }
 
     private static void courseUpdater(long initDelay) {
         POOL.scheduleAtFixedRate(() -> {
+            if (isHoliday) {
+                return;
+            }
             for (Map.Entry<Long, Queue<CourseDto>> queueEntry : CourseData.USER_TODAY_COURSE.entrySet()) {
                 queueEntry.getValue().clear();
                 CourseData.refreshQueue(queueEntry.getKey());
                 if (queueEntry.getValue().isEmpty()) {
-                    Bot.getInstance(ConfigFile.CONFIG.getBot()).getFriend(queueEntry.getKey()).sendMessage("早上好\n" + FormatterUtil.getDateTitleInfo(LocalDate.now()) + "\n" + "今日无课，好好休息");
                     continue;
                 }
                 String courseInfo = FormatterUtil.normalFormat(LocalDate.now(), queueEntry.getValue(), "早上好");
@@ -51,6 +59,9 @@ public class Timer {
 
     private static void nextCourseChecker(long initDelay) {
         POOL.scheduleAtFixedRate(() -> {
+            if (isHoliday) {
+                return;
+            }
             for (Map.Entry<Long, Queue<CourseDto>> queueEntry : CourseData.USER_TODAY_COURSE.entrySet()) {
                 if (queueEntry.getValue().isEmpty() ||!CourseUtil.isCourseStart(queueEntry.getValue().peek())) {
                     continue;
@@ -61,4 +72,16 @@ public class Timer {
         }, initDelay, 60 * 1000, TimeUnit.MILLISECONDS);
     }
 
+    private static void holidayChecker(long initDelay) {
+        POOL.scheduleAtFixedRate(Timer::isHoliday, initDelay, 24 * 60 * 60 * 1000, TimeUnit.MILLISECONDS);
+    }
+
+    private static void isHoliday() {
+        Result<Boolean> rs = HttpUtils.sendHolidayReq(LocalDate.now());
+        if (rs.getCode() == StatusCodeConstants.HOLIDAY_GET_FAILED) {
+            isHoliday = false;
+            return;
+        }
+        isHoliday = rs.getData();
+    }
 }
